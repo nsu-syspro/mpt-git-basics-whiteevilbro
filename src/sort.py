abe0ac3586fb6c0734baf885a702e4456f0cf111
Ivan Trepakov <liontiger23@gmail.com>
2024-04-21 14:27:10 +0700

    Make sorting more verbose

--- src/sort.py
+++ src/sort.py
@@ -5,6 +5,7 @@ for i in range(1, len(arr)):
     a_i = arr[i]
     j = i - 1
     while j >= 0:
+        print("comparing {} and {}".format(arr[j], a_i))
         if arr[j] > a_i:
             arr[j + 1] = arr[j]
         else:
