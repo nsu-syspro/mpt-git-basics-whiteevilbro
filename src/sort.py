e7a8f53f216da7e65b4591d661c8a90f3a27f1c3
Ivan Trepakov <liontiger23@gmail.com>
2024-04-21 14:27:59 +0700

    Use better sorting algorithm
    However it still has quadratic complexity

--- src/sort.py
+++ src/sort.py
@@ -1,11 +1,15 @@
 arr = [40, 4, 20, 10, 30, 6, 10]
 
-# selection sort:
-for i in range(len(arr) - 1):
-    min = i
-    for j in range(i + 1, len(arr)):
-        if arr[j] < arr[min]:
-            min = j
-    arr[i], arr[min] = arr[min], arr[i]
+# insertion sort:
+for i in range(1, len(arr)):
+    a_i = arr[i]
+    j = i - 1
+    while j >= 0:
+        if arr[j] > a_i:
+            arr[j + 1] = arr[j]
+        else:
+            break
+        j -= 1
+    arr[j + 1] = a_i
 
 print(arr)
