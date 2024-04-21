69079b48af3b80b99c14b69c666609925665bf17
Ivan Trepakov <liontiger23@gmail.com>
2024-04-21 14:25:59 +0700

    Implement base sorting

---
+++ src/sort.py
@@ -0,0 +1,11 @@
+arr = [40, 4, 20, 10, 30, 6, 10]
+
+# selection sort:
+for i in range(len(arr) - 1):
+    min = i
+    for j in range(i + 1, len(arr)):
+        if arr[j] < arr[min]:
+            min = j
+    arr[i], arr[min] = arr[min], arr[i]
+
+print(arr)
