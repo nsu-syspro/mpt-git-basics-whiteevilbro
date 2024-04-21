f96816b030df1e86f79404ebf8a7b0f5531ab3ea
Ivan Trepakov <liontiger23@gmail.com>
2024-04-21 14:27:10 +0700

    Make sorting more verbose

--- src/sort.py
+++ src/sort.py
@@ -4,6 +4,7 @@ arr = [40, 4, 20, 10, 30, 6, 10]
 for i in range(len(arr) - 1):
     min = i
     for j in range(i + 1, len(arr)):
+        print("comparing {} and {}".format(arr[j], arr[min]))
         if arr[j] < arr[min]:
             min = j
     arr[i], arr[min] = arr[min], arr[i]
